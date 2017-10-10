fe7c6f2236b41c3f1a0fe7a36a70790dda685f14
marcin <dupa@gmail.com>
2017-10-10 23:05:29 +0200

    Adding first program

---
+++ Chapter_02/exercise2_1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : exercise2_1
+// Author      : Marcin214
+// Description : Program which displays your name and address
+//============================================================================
+
+#include <iostream>
+
+int main(){
+	using namespace std;
+	cout << "Jan Kowalski" <<endl;
+	cout << "00-000 Cracow" <<endl;
+
+	return 0;
+}
